95cc0978712bcf2542644207d8fbc5913c2a5b5c
Emil Karlsson <own.emka@gmail.com>
2020-05-29 23:47:07 +0200

    UDPSync is now always sent upon receiving ID

--- src/netmodules/SetupModule.cpp
+++ src/netmodules/SetupModule.cpp
@@ -11,6 +11,7 @@ void SetupModule::HandlePacket(UID, const ParsedPacket &packet)
 {
     sf::Uint64 uid = *reinterpret_cast<sf::Uint64 *>(packet.data.get());
     m_ioHelper->SetUID(uid);
+    m_ioHelper->SendEmpty<UDP>(UDPSync, packet.connection);
 }
 
 void SetupModule::HandlePacket(UDPSync, const ParsedPacket &packet)
